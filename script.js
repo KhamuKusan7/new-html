cd47cc186814f4cfe19691ac146fd8f873bab79e
KhamuKusan7 <kusanshrestha@gmail.com>
2024-02-14 10:03:02 +0545

    Made some display changes

---
+++ script.js
@@ -0,0 +1,20 @@
+// let a=10;
+// let b=50;
+// let sum=a+b;
+
+// document.write(sum);
+
+// alert("Welcome to the Premier League");
+
+// let fname="kusan";
+// let lname="khamu";
+// document.write(fname);
+
+let country="Nepal";
+if(country="Nepal")
+{
+    document.write("You are Nepali");
+}
+else {
+    document.write("You are not Nepali");
+}
